fd42e747fcc810390fdcfd3afb0c021268da621a
superuser <superuser@Ubuntu.myguest.virtualbox.org>
2026-06-14 14:52:50 +0500

    upd 4

--- unit-test.sh
+++ unit-test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Run unit tests for the assignment
+set -e
 
 # Automate these steps from the readme:
 # Create a build subdirectory, change into it, run
 # cmake .. && make && run the assignment-autotest application
 mkdir -p build
 cd build
+
 cmake ..
-make clean
 make
+
 cd ..
 ./build/assignment-autotest/assignment-autotest
