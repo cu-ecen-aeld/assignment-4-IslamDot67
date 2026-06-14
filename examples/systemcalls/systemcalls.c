0aeb97217a00f5cfb967bf681902b1efb7295b65
IslamDot67 <kkek1569@gmail.com>
2026-06-14 16:54:24 +0500

    upd 5

---
+++ examples/systemcalls/systemcalls.c
@@ -0,0 +1,146 @@
+#include "systemcalls.h"
+
+/**
+ * @param cmd the command to execute with system()
+ * @return true if the command in @param cmd was executed
+ *   successfully using the system() call, false if an error occurred,
+ *   either in invocation of the system() call, or if a non-zero return
+ *   value was returned by the command issued in @param cmd.
+*/
+bool do_system(const char *cmd)
+{
+    if (cmd == NULL) 
+    {
+        return false;
+    }
+    int ret = system(cmd);
+    return (ret == -1) ? false : true;
+}
+
+/**
+* @param count -The numbers of variables passed to the function. The variables are command to execute.
+*   followed by arguments to pass to the command
+*   Since exec() does not perform path expansion, the command to execute needs
+*   to be an absolute path.
+* @param ... - A list of 1 or more arguments after the @param count argument.
+*   The first is always the full path to the command to execute with execv()
+*   The remaining arguments are a list of arguments to pass to the command in execv()
+* @return true if the command @param ... with arguments @param arguments were executed successfully
+*   using the execv() call, false if an error occurred, either in invocation of the
+*   fork, waitpid, or execv() command, or if a non-zero return value was returned
+*   by the command issued in @param arguments with the specified arguments.
+*/
+
+bool do_exec(int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+    char * command[count+1];
+    int i;
+    for(i=0; i<count; i++)
+    {
+        command[i] = va_arg(args, char *);
+    }
+    command[count] = NULL;
+    // this line is to avoid a compile warning before your implementation is complete
+    // and may be removed
+    command[count] = command[count];
+    va_end(args);
+
+
+    fflush(stdout);
+    pid_t pid=fork();
+    if (pid<0)
+    {
+        perror("fork Error: ");
+        return false;
+    }
+    else if (pid==0)//child process
+    {
+        execv(command[0],command);
+        //if execv failed
+        perror("exec Error: ");
+        exit(EXIT_FAILURE);
+    }
+    else//parent process
+    {
+        int status;
+        waitpid(pid, &status, 0);
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) 
+        {
+            return true; 
+        } 
+        else 
+        {
+            return false;
+        }
+        
+    }
+    
+    return true;
+}
+
+/**
+* @param outputfile - The full path to the file to write with command output.
+*   This file will be closed at completion of the function call.
+* All other parameters, see do_exec above
+*/
+bool do_exec_redirect(const char *outputfile, int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+    char * command[count+1];
+    int i;
+    for(i=0; i<count; i++)
+    {
+        command[i] = va_arg(args, char *);
+    }
+    command[count] = NULL;
+    // this line is to avoid a compile warning before your implementation is complete
+    // and may be removed
+    command[count] = command[count];
+    va_end(args);
+
+    fflush(stdout);
+    pid_t pid=fork();
+    if (pid<0)
+    {
+        perror("fork Error: ");
+        return false;
+    }
+    else if (pid==0)//child process
+    {
+        int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd < 0) {
+            perror("Open file Failed: ");
+            exit(EXIT_FAILURE);
+        }
+        if (dup2(fd, STDOUT_FILENO) < 0) {
+            perror("dup2 redirection Failed: ");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        close(fd);
+        execv(command[0], command);
+        perror("exec Error: ");
+        exit(EXIT_FAILURE);
+    }
+    else//parent process
+    {
+        int status;
+        waitpid(pid, &status, 0);
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) 
+        {
+            return true; 
+        } 
+        else 
+        {
+            return false;
+        }
+        
+    }
+
+    return true;
+}
---
+++ examples/systemcalls/systemcalls.h
@@ -0,0 +1,14 @@
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <stdbool.h>    
+#include <stdarg.h>     
+#include <unistd.h>     
+#include <sys/wait.h>   
+#include <sys/types.h>  
+#include <fcntl.h>      
+
+bool do_system(const char *command);
+
+bool do_exec(int count, ...);
+
+bool do_exec_redirect(const char *outputfile, int count, ...);
